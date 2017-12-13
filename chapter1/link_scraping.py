9ba6a5ef7108324269a5da971eed1b47208268d5
zhangminvip <2107609379@qq.com>
2017-12-13 20:41:37 +0800

    throttle

--- chapter1/link_scraping.py
+++ chapter1/link_scraping.py
@@ -6,6 +6,44 @@ import urlparse
 import robotparser
 
 
+class Throttle:
+    '''
+    Add a delay between downloads to the same domain
+    
+    
+    throttle = Throttle(delay)
+    ...
+    throttle.wait(url)
+    
+    
+    '''
+    
+    def __init__(self, delay):
+        # amount fo delay between downloads for each domain
+        self.delay = delay
+        # timestamp of when a domain was last accessed
+        self.domains = {}
+        
+        
+    def wait(self, url):
+        domain = urlparse.urlparse(url).netloc
+        last_accessed = self.domains.get(domain)
+        
+        if self.delay > 0 and last_accessed is not None:
+            sleep_secs = self.delay - (datetime.datetime.now() - last_accessed).seconds
+            
+            if sleep_secs > 0:
+                # domain has been accessed recently
+                # so need to sleep
+                
+                time.sleep(sleep_secs)
+                
+        # update the last accessed  time
+        self.domains[domain] = datetime.datetime.now()
+        
+        
+
+
 def download(url, user_agent = 'wswp', num_retries = 2,proxy = None):
     
     print 'Downloading', url
